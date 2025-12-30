872734090e5df6477bc769cf3158e4b9dac2ce5a
mmiaaub22 <breath20292@blindbuster.eml.monster>
2025-12-30 23:37:34 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+import React from "react";
+import AttackPanel from "../components/AttackPanel";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <AttackPanel />
+    </div>
+  );
+}
